86ec753b9fb9968ee208f0055cd399eb40b5776b
Stefano <hemetmas@gmail.com>
2024-11-28 14:56:43 +0100

    ok

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
-
+import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
 
-// https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [
+    react({}),    // React debe ser pasado como un objeto (aunque no tiene opciones)
+    tailwind({}),  // Tailwind también debe ser pasado como un objeto
+  ],
 });
